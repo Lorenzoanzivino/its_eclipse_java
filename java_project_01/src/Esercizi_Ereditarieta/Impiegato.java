6c5b2fd3443a65d789ef2f891a9b9ca38917d0f1
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2026-01-09 18:44:37 +0100

    Aggiornamento progetti src

--- java_project_01/src/Esercizi_Ereditarieta/Impiegato.java
+++ java_project_01/src/Esercizi_Ereditarieta/Impiegato.java
@@ -1,6 +1,7 @@
 package Esercizi_Ereditarieta;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Impiegato implements Comparable<Impiegato> {
 
@@ -48,6 +49,23 @@ public class Impiegato implements Comparable<Impiegato> {
 	public int compareTo(Impiegato o) {
 		// Criterio che induce l'ordinamento crescente per nome
 		return this.nome.compareTo(o.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Impiegato other = (Impiegato) obj;
+		return Objects.equals(nome, other.nome);
 	}
 
 }
